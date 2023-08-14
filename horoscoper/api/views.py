9bd5dcf898fa9e21ebbd64d3e3521f1b0aee2f8e
Viacheslav Greshilov <slovaricheg@gmail.com>
2023-08-14 13:49:46 +0300

    add infer response latency metrics

--- horoscoper/api/views.py
+++ horoscoper/api/views.py
@@ -1,9 +1,11 @@
 import logging
+import time
 from pathlib import Path
 
 from fastapi import APIRouter, HTTPException, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from prometheus_client import Counter, Histogram
 from pydantic import BaseModel, Field
 from sse_starlette.sse import EventSourceResponse, ServerSentEvent
 
@@ -15,9 +17,16 @@ from .state import State
 
 API_DIR = Path(__file__).parent
 
+infer_first_response = Histogram(
+    "api_infer_first_response", "Infer API first response latency"
+)
+infer_messages_count = Counter(
+    "api_infer_messages_count", "InferMessage counter in API", ["status"]
+)
+
+logger = logging.getLogger(__name__)
 router = APIRouter()
 templates = Jinja2Templates(directory=API_DIR / "templates")
-logger = logging.getLogger(__name__)
 
 
 @router.get("/", response_class=HTMLResponse, include_in_schema=False)
@@ -40,7 +49,11 @@ async def infer(request: Request, state: State, infer_request: APIInferRequest):
     context = LLMContext(prefix=infer_request.prefix)
     await state.batcher.add_context_to_batch(context)
 
+    start_infer = time.monotonic()
+
     async def iter_infer_response():
+        first_message = True
+
         async with state.redis.pubsub() as pubsub:
             await pubsub.subscribe(context.redis_key)
 
@@ -58,6 +71,9 @@ async def infer(request: Request, state: State, infer_request: APIInferRequest):
                         error="Timeout inference",
                     )
                     yield ServerSentEvent(data=error_msg.model_dump_json())
+                    infer_messages_count.labels(
+                        status=str(InferMessageStatus.ERROR)
+                    ).inc()
                     return
 
                 if raw_message["type"] == "subscribe":
@@ -65,6 +81,11 @@ async def infer(request: Request, state: State, infer_request: APIInferRequest):
 
                 json_data = raw_message["data"]
                 infer_message = InferMessage.model_validate_json(json_data)
+                infer_messages_count.labels(status=str(infer_message.status)).inc()
+
+                if first_message:
+                    infer_first_response.observe(time.monotonic() - start_infer)
+                    first_message = False
 
                 yield ServerSentEvent(data=json_data.decode())
                 if infer_message.status in (
